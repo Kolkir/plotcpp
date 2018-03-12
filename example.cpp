6594d60e51ce005b6ce45df6a309f1511e6e26a6
Kolkir <rotate@ukr.net>
2018-03-12 23:27:29 +0200

    Move utility functions to separate files

--- example.cpp
+++ example.cpp
@@ -4,33 +4,15 @@
 #include <vector>
 
 #include "plot.h"
+#include "utils.h"
 
-template <typename T> auto LinSpace(T a, T b, size_t n) {
-  auto h = (b - a) / static_cast<T>(n - 1);
-  return [ val = a, h ]() mutable {
-    val += h;
-    return val;
-  };
-}
+const bool persist_gnuplot_window = true;
+const size_t n = 50;
 
-int main() {
-  using plotcpp::Lines;
-  using plotcpp::Plot;
-  using plotcpp::Points;
-  using std::generate;
-  using std::transform;
-  using std::vector;
+void PlotAutoscale(const std::vector<float>& y, const std::vector<float>& x) {
+  using namespace plotcpp;
 
-  const size_t n = 50;
-  const float pi = M_PI;
-
-  vector<float> x(n);
-  generate(x.begin(), x.end(), LinSpace(-pi, pi, n));
-  vector<float> y(n);
-  std::transform(x.begin(), x.end(), y.begin(),
-                 [](auto v) { return std::sin(v); });
-
-  Plot plt;
+  Plot plt(persist_gnuplot_window);
   plt.SetTerminal("qt");
   plt.SetTitle("Sine");
   plt.SetXLabel("X");
@@ -41,6 +23,114 @@ int main() {
              Points(x.begin(), x.end(), y.begin(), "points"));
 
   plt.Flush();
+}
 
+void PlotXYRanges(const std::vector<float>& y, const std::vector<float>& x) {
+  using namespace plotcpp;
+
+  Plot plt(persist_gnuplot_window);
+  plt.SetTerminal("qt");
+  plt.SetTitle("Sine");
+  plt.SetXLabel("X");
+  plt.SetYLabel("Y");
+  plt.SetXRange(-pi / 2, pi / 2);
+  plt.SetYRange(-0.5, 0.5);
+
+  plt.Draw2D(Lines(x.begin(), x.end(), y.begin(), "lines"),
+             Points(x.begin(), x.end(), y.begin(), "points"));
+
+  plt.Flush();
+}
+
+void PlotToFile(const std::vector<float>& y, const std::vector<float>& x) {
+  using namespace plotcpp;
+
+  Plot plt;
+  plt.SetTerminal("png");
+  plt.SetOutput("test.png");
+  plt.SetTitle("Sine");
+  plt.SetXLabel("X");
+  plt.SetYLabel("Y");
+  plt.SetAutoscale();
+
+  plt.Draw2D(Lines(x.begin(), x.end(), y.begin(), "lines"),
+             Points(x.begin(), x.end(), y.begin(), "points"));
+
+  plt.Flush();
+  std::cout << "Plot saved to test.png\n";
+}
+
+void MultiPlot(const std::vector<float>& x) {
+  using namespace plotcpp;
+
+  std::vector<float> y_sin(n);
+  std::transform(x.begin(), x.end(), y_sin.begin(),
+                 [](auto v) { return std::sin(v); });
+  std::vector<float> y_cos(n);
+  std::transform(x.begin(), x.end(), y_cos.begin(),
+                 [](auto v) { return std::cos(v); });
+  std::vector<float> y_tan(n);
+  std::transform(x.begin(), x.end(), y_tan.begin(),
+                 [](auto v) { return std::tan(v); });
+  std::vector<float> y_tanh(n);
+  std::transform(x.begin(), x.end(), y_tanh.begin(),
+                 [](auto v) { return std::tanh(v); });
+
+  Plot plt(persist_gnuplot_window);
+  plt.SetTerminal("qt");
+  plt.SetXLabel("x");
+  plt.SetYLabel("y");
+  plt.SetAutoscale();
+
+  plt.GnuplotCommand("set multiplot layout 2,2 rowsfirst");
+  plt.Draw2D(Lines(x.begin(), x.end(), y_sin.cbegin(), "sin"));
+  plt.Draw2D(Lines(x.begin(), x.end(), y_cos.cbegin(), "cos"));
+  plt.Draw2D(Lines(x.begin(), x.end(), y_tan.cbegin(), "tan"));
+  plt.Draw2D(Lines(x.begin(), x.end(), y_tanh.cbegin(), "tanh"));
+  plt.GnuplotCommand("unset multiplot");
+  plt.Flush();
+}
+
+void ShowHelp(const char* prog_path) {
+  std::cout << prog_path
+            << " [n]\n"
+               "where n:\n"
+               ""
+               "0 - Simple Autoscale plot\n"
+               "1 - Simple plot with x,y ranges\n"
+               "2 - Plot to file\n"
+               "3 - Multiplot with custom gnuplot commands\n";
+}
+
+int main(int argc, char* argv[]) {
+  using namespace std;
+
+  vector<float> x(n);
+  generate(x.begin(), x.end(), LinSpace(-pi, pi, n));
+  vector<float> y(n);
+  std::transform(x.begin(), x.end(), y.begin(),
+                 [](auto v) { return std::sin(v); });
+
+  if (argc != 2) {
+    ShowHelp(argv[0]);
+  } else {
+    auto ex = argv[1][0] - '0';
+    switch (ex) {
+      case 0:
+        PlotAutoscale(y, x);
+        break;
+      case 1:
+        PlotXYRanges(y, x);
+        break;
+      case 2:
+        PlotToFile(y, x);
+        break;
+      case 3:
+        MultiPlot(x);
+        break;
+      default:
+        ShowHelp(argv[0]);
+    }
+  }
   return 0;
 }
--- utils.h
+++ utils.h
@@ -1,4 +1,15 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#endif // UTILS_H
+inline const float pi = 3.14159265358979323846;
+
+template <typename T>
+auto LinSpace(T a, T b, size_t n) {
+  auto h = (b - a) / static_cast<T>(n - 1);
+  return [ val = a, h ]() mutable {
+    val += h;
+    return val;
+  };
+}
+
+#endif  // UTILS_H
